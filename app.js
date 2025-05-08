bf0352759d719d95a522e09eed2532957201998c
Prathamesh Borade <pborade90@gmail.com>
2025-05-08 22:38:50 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Serve static files
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
